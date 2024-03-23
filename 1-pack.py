c4f9f713550ce09c39b8aa5f2dcda5c15e62dfc6
myleslipford <myles.lipford12@gmail.com>
2024-03-22 19:58:36 -0500

    wrote the code for the entry box at the top and frames for buttons below it

--- 1-pack.py
+++ 1-pack.py
@@ -1,6 +1,14 @@
 from tkinter import *
 from tkinter import ttk
-
 root = Tk()
+
+# Entry box for displaying the expression
+entry = Entry(root, width=20, borderwidth=5, state="disabled")
+entry.pack(side=TOP)
+
+# Frame for buttons
+button_frame = Frame(root)
+button_frame.pack(side=TOP)
+
 
 root.mainloop()
