267420133be18baeae3ad5c7e72db04f876ebef5
myleslipford <myles.lipford12@gmail.com>
2024-03-22 22:34:42 -0500

    made changes to code in grid.py

--- 2-grid.py
+++ 2-grid.py
@@ -3,12 +3,23 @@ from tkinter import ttk
 root = Tk()
 
 
-Label(root, text="Name:").grid(row=0, column=0, sticky="e")
-Label(root, text="Email:").grid(row=1, column=0, sticky="e")
-Label(root, text="Password:").grid(row=2, column=0, sticky="e")
+name = ttk.Label(root, text="Name:")
+name.grid(row=0, column=0, sticky="e")
 
+email = ttk.Label(root, text="Email:")
+email.grid(row=1, column=0, sticky="e")
 
-sign_up_button = Button(root, text="Sign Up Now")
+password = ttk.Label(root, text="Password:")
+password.grid(row=2, column=0, sticky="e")
+
+name_entry = Entry(root)
+email_entry = Entry(root)
+password_entry = Entry(root)  
+name_entry.grid(row=0, column=1)
+email_entry.grid(row=1, column=1)
+password_entry.grid(row=2, column=1)
+
+sign_up_button = ttk.Button(root, text="Sign Up Now")
 sign_up_button.grid(row=3, columnspan=2)
 
 root.mainloop()
