d31d956f72cd1b8c8ed252bca84960fbe9cacafc
myleslipford <myles.lipford12@gmail.com>
2024-03-22 20:09:38 -0500

    wrote the code for button 7,8,9,plus and tested to make sure it worked

--- 1-pack.py
+++ 1-pack.py
@@ -7,8 +7,24 @@ entry = Entry(root, width=20, borderwidth=5, state="disabled")
 entry.pack(side=TOP)
 
 # Frame for buttons
-button_frame = Frame(root)
-button_frame.pack(side=TOP)
+button_frame1 = Frame(root)
+button_frame1.pack(side=TOP)
+
+button_row1 = Frame(button_frame1)
+button_row1.pack(side=TOP)
+
+button7 = ttk.Button(button_row1, text="7")
+button7.pack(side=LEFT)
+
+button8 = ttk.Button(button_row1, text="8")
+button8.pack(side=LEFT)
+
+button9 = ttk.Button(button_row1, text="9")
+button9.pack(side=LEFT)
+
+button_plus = ttk.Button(button_row1, text="+")
+button_plus.pack(side=LEFT)
+
 
 
 root.mainloop()
